dd71d775bb1da280be617e1ddfbaf60ae34ea959
surajit-singha-sisir <sci.surajit@gmail.com>
2024-03-21 18:59:46 +0600

    Catergory page designed using Mastors CDN

--- script.js
+++ script.js
@@ -109,3 +109,91 @@ $(document).ready(function () {
     $(".progress-bar__mask").width(scrollPercent + "%");
   });
 });
+
+
+// HAMBURGER REVEAL
+function toggleMenu() {
+  $('.hamburger').toggleClass('fa-bars fa-x');
+  $('.fullResNav').toggleClass('show');
+}
+
+//TAB TOGGLE
+$(document).ready(function () {
+      $(".tab-btn").click(function () {
+          $(".tab-btn").removeClass('active');
+
+      });
+  });
+
+
+
+$(document).ready(function () {
+  $('#searchIcon').click(function () {
+    $('.search-input').toggleClass('active'); // Toggle the 'active' class to show/hide input
+    $(this).toggleClass('fa-search fa-x'); // Toggle the search icon to cross
+  });
+
+  $('#closeIcon').click(function () {
+    $('.search-input').removeClass('active'); // Hide the input when close icon is clicked
+    $('#searchIcon').removeClass('fa-x').addClass('fa-search'); // Change the cross back to search icon
+  });
+});
+
+$(document).ready(function() {
+  // Smooth scrolling for all anchor links
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 6000); // Adjust the duration for slower or faster scrolling
+      }
+  });
+});
+
+// GALLERY SCECTION
+
+// Active span change Event
+$(document).ready(function () {
+$(".gallery-item span").on("click", function () {
+  $(".gallery-item span").removeClass("active");
+  $(this).addClass("active");
+});
+
+// 3 Second scroll
+function toggleActive() {
+  var $active = $(".gallery-item .active");
+  var $next = $active.next().length
+    ? $active.next()
+    : $(".gallery-item span").first();
+
+  // Check if viewport width is 768px or less
+  if ($(window).width() <= 768) {
+    // Fade out the current active span
+    $active.fadeOut("slow", function() {
+      $active.removeClass("active");
+      // Fade in the next span and add the active class
+      $next.fadeIn("slow").addClass("active");
+    });
+  } 
+  else {
+    // No transition for larger devices, simply change the active class
+    $active.removeClass("active");
+    $next.addClass("active");
+  }
+}
+
+// Set interval to change active span every 3 seconds
+var interval = setInterval(toggleActive, 3000);
+
+// Pause on mouse hover
+$(".gallery-item").hover(
+  function () {
+    clearInterval(interval);
+  },
+  function () {
+    interval = setInterval(toggleActive, 3000);
+  }
+);
+});
